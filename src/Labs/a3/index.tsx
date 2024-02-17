a65898728017b03c1b32abb9f12ebacf9e07fc4e
christinaw20 <68133503+christinaw20@users.noreply.github.com>
2024-02-16 22:29:45 -0500

    started lab

---
+++ src/Labs/a3/index.tsx
@@ -0,0 +1,13 @@
+import JavaScript from "./JavaScript";
+
+function Assignment3() {
+    return (
+      <div className="container m-0">
+        <h1>Assignment 3</h1>
+        <JavaScript/>
+      </div>
+    );
+  }
+  export default Assignment3;
+  
+  
